7c6b7dd2cce8b66dd599b1b79c34c4ef3f33e9bc
Thermatix <mbeckerwork@gmail.com>
2015-01-19 10:17:10 +0000

    De-couples logging class from info class

--- app/bps.org/lib/logging.rb
+++ app/bps.org/lib/logging.rb
@@ -2,14 +2,11 @@ require 'logger'
 
 module BPS
   class Log
-    F = File.open(Info[:logfile], 'a')
-    F.sync = true
-    @l = Logger.new(F).tap do |log|
-      log.progname = Info[:short_name]
-    end
     class << self
+      attr_accessor :logger,:file 
+       
       def set_level(level)
-        @l.level = Logger.const_get level.upcase
+        self.logger.level = Logger.const_get level.upcase
       end
 
       def display(message, level)
@@ -20,24 +17,32 @@ module BPS
         case message.class.to_s
           when 'String'
             display message, level
-            @l.send level, message
+            self.logger.send level, message
           when 'Array'
             message.each do |line|
               display line, level
-              @l.send level, line
+              self.logger.send level, line
             end
           when 'Hash'
             message.each do |level,line|
               display line, level
-              @l.send level, line
+              self.logger.send level, line
             end
         else
           raise TypeError, "method expects Hash,String or Array, message was of type #{message.class}"
         end
       end
       
-      
+      def create name,file
+        self.file = File.open(file, 'a')
+        self.file.sync = true
+        self.logger = Logger.new(self.file).tap do |log|
+          log.progname = name
+        end
+      end
+
     end
+   
 
   end
 
